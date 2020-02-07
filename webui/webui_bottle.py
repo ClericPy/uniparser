79adfebdc726b9dcfb9a50fef2cc33397db04780
clericpy <lidongone@qq.com>
2020-02-07 15:11:19 +0800

    update element-ui for drawer mask layer bug

--- webui/webui_bottle.py
+++ webui/webui_bottle.py
@@ -14,10 +14,10 @@ uni = Uniparser()
 GLOBAL_RESP = None
 
 cdn_urls = dict(
-    VUE_JS_CDN='https://cdn.staticfile.org/vue/2.6.10/vue.min.js',
+    VUE_JS_CDN='https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
     ELEMENT_CSS_CDN=
-    'https://cdn.staticfile.org/element-ui/2.11.1/theme-chalk/index.css',
-    ELEMENT_JS_CDN='https://cdn.staticfile.org/element-ui/2.11.1/index.js',
+    'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
+    ELEMENT_JS_CDN='https://cdn.staticfile.org/element-ui/2.13.0/index.js',
     VUE_RESOURCE_CDN=
     'https://cdn.staticfile.org/vue-resource/1.5.1/vue-resource.min.js')
 

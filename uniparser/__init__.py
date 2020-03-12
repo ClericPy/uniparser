4f2d639c0f5274200d9d32b49b60014fc3c7183f
clericpy <lidongone@qq.com>
2020-03-12 22:09:00 +0800

    release stable 1.3.0

--- uniparser/__init__.py
+++ uniparser/__init__.py
@@ -7,6 +7,6 @@ from .utils import (AiohttpAsyncAdapter, AsyncRequestAdapter,
                     SyncRequestAdapter, TorequestsAsyncAdapter,
                     get_available_async_request, get_available_sync_request)
 
-__all__ = ['Uniparser', 'ParseRule', 'CrawlerRule', 'HostRule']
-__version__ = '1.2.9'
 logging.getLogger('uniparser').addHandler(logging.NullHandler())
+__all__ = ['Uniparser', 'ParseRule', 'CrawlerRule', 'HostRule']
+__version__ = '1.3.0'

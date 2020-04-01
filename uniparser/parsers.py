4a1fe52623751aec4dec28a451b0ad13c3add1cb
clericpy <clericpy@gmail.com>
2020-04-01 12:25:17 +0800

    remove default UA from crawlerrule

--- uniparser/parsers.py
+++ uniparser/parsers.py
@@ -813,11 +813,6 @@ class CrawlerRule(JsonSerializable):
                  context: dict = None,
                  **kwargs):
         _request_args: dict = ensure_request(request_args)
-        if _request_args:
-            _request_args["headers"] = _request_args.setdefault(
-                "headers", {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
-                })
         self.context = context or {}
         parse_rules = [
             ParseRule(context=self.context, **parse_rule)

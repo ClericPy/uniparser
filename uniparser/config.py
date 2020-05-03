12dadf96cd1f0ba8c1f661cf3eb263506d556175
clericpy <clericpy@gmail.com>
2020-05-03 15:29:37 +0800

    fix some lintings

--- uniparser/config.py
+++ uniparser/config.py
@@ -11,3 +11,10 @@ class GlobalConfig:
     __request__ = '__request__'
     __result__ = '__result__'
     __encoding__ = 'utf-8'
+    cdn_urls = {
+        'VUE_JS_CDN': 'https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
+        'ELEMENT_CSS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
+        'ELEMENT_JS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/index.js',
+        'VUE_RESOURCE_CDN': 'https://cdn.staticfile.org/vue-resource/1.5.1/vue-resource.min.js',
+        'CLIPBOARDJS_CDN': 'https://cdn.staticfile.org/clipboard.js/2.0.4/clipboard.min.js',
+    }

b51df437432bc72218c35524f48bb2e9075429b7
clericpy <clericpy@gmail.com>
2020-05-03 15:05:30 +0800

    parser support __call__ method

--- test_parsers.py
+++ test_parsers.py
@@ -496,6 +496,14 @@ def test_python_parser():
     # print(result)
     assert result == 'abc'
 
+    # ===================== test parser.__call__() =====================
+    result = uni.python('abc', 'base64_encode', '')
+    # print(result)
+    assert result == 'YWJj'
+    result = uni.python('YWJj', 'base64_decode', '')
+    # print(result)
+    assert result == 'abc'
+
 
 def test_udf_parser():
     uni = Uniparser()
@@ -1099,22 +1107,22 @@ if __name__ == "__main__":
     from uniparser.config import GlobalConfig
     GlobalConfig.GLOBAL_TIMEOUT = 5
     for case in (
-            # test_css_parser,
-            # test_xml_parser,
-            # test_re_parser,
-            # test_jsonpath_parser,
-            # test_objectpath_parser,
-            # test_jmespath_parser,
+            test_css_parser,
+            test_xml_parser,
+            test_re_parser,
+            test_jsonpath_parser,
+            test_objectpath_parser,
+            test_jmespath_parser,
             test_python_parser,
-            # test_udf_parser,
-            # test_loader_parser,
-            # test_time_parser,
-            # test_uni_parser,
-            # test_crawler_rule,
-            # test_default_usage,
-            # test_crawler_storage,
-            # test_uni_parser_frequency,
-            # test_crawler,
+            test_udf_parser,
+            test_loader_parser,
+            test_time_parser,
+            test_uni_parser,
+            test_crawler_rule,
+            test_default_usage,
+            test_crawler_storage,
+            test_uni_parser_frequency,
+            test_crawler,
     ):
         case()
         print(case.__name__, 'ok')

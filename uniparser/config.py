b0d329ddc3897bd5a2a89ecbf35882b1f6670606
clericpy <clericpy@gmail.com>
2020-09-22 23:11:00 +0800

    use cdnjs

--- uniparser/config.py
+++ uniparser/config.py
@@ -12,11 +12,11 @@ class GlobalConfig:
     __result__ = '__result__'
     __encoding__ = 'utf-8'
     cdn_urls = {
-        'VUE_JS_CDN': 'https://cdn.staticfile.org/vue/2.6.11/vue.min.js',
-        'ELEMENT_CSS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/theme-chalk/index.css',
-        'ELEMENT_JS_CDN': 'https://cdn.staticfile.org/element-ui/2.13.0/index.js',
-        'VUE_RESOURCE_CDN': 'https://cdn.staticfile.org/vue-resource/1.5.1/vue-resource.min.js',
-        'CLIPBOARDJS_CDN': 'https://cdn.staticfile.org/clipboard.js/2.0.4/clipboard.min.js',
+        'VUE_JS_CDN': 'https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.min.js',
+        'ELEMENT_CSS_CDN': 'https://cdnjs.cloudflare.com/ajax/libs/element-ui/2.13.2/theme-chalk/index.css',
+        'ELEMENT_JS_CDN': 'https://cdnjs.cloudflare.com/ajax/libs/element-ui/2.13.2/index.js',
+        'VUE_RESOURCE_CDN': 'https://cdnjs.cloudflare.com/ajax/libs/vue-resource/1.5.1/vue-resource.min.js',
+        'CLIPBOARDJS_CDN': 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.6/clipboard.min.js',
     }
     DEFAULT_UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
     demo_choices = [

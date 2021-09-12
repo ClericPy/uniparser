d9510fd8df925d1a30e5cd71790bdbf474781b58
clericpy <clericpy@gmail.com>
2021-09-12 23:45:53 +0800

    fix attribute error for asyncio.get_running_loop

--- uniparser/utils.py
+++ uniparser/utils.py
@@ -29,7 +29,10 @@ except ImportError:
 
     async def to_thread(func, *args, **kwargs):
         """copy python3.9"""
-        loop = asyncio.get_running_loop()
+        try:
+            loop = asyncio.get_running_loop()
+        except AttributeError:
+            loop = asyncio.get_event_loop()
         ctx = copy_context()
         func_call = partial(ctx.run, func, *args, **kwargs)
         return await loop.run_in_executor(None, func_call)
